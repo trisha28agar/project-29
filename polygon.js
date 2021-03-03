b18466e6fd8cceea332743b4743177d42de1cdd9
trisha28agar <73343319+trisha28agar@users.noreply.github.com>
2021-03-03 15:36:34 +0530

    uploaded changes

---
+++ polygon.js
@@ -0,0 +1,25 @@
+class poly{
+
+    constructor(x, y, width, height) {
+        var options = {
+
+            isStatic: false,
+            restitution: 0.1,
+            frictions: 0.01,
+            density:0.02
+
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+
+    display() {
+        strokeWeight(2);
+        fill("yellow");
+        rect(position.x, position.y, this.width, this.height);
+
+    }
+}
